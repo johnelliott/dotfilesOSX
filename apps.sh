fe5892475e4aecf17a1576e35ac0e1fd11b6d986
John Elliott <johnelliott703@gmail.com>
2014-07-15 23:41:07 -0400

    testing different terminal colors and general changes, path still messed up

---
+++ apps.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install more apps
+
+# Install EMACS
+brew install emacs --use-git-head --HEAD
+
+# Setup git
+git config --global core.editor emacs
+
+# Install jshint to allow checking of JS code within emacs                                                                                                                                                                                 
+# http://jshint.com/                                                                                                                                                                                                                                    
+npm install -g jshint
+
+# Install Grunt for automated node builds                                                                                                                                                                                                               
+# http://gruntjs.com/getting-started for details                                                                                                                                                                                                        
+npm install -g grunt-cli
+npm install -g grunt-init
+
+# Install Bower for front-end package management                                                                                                                                                                                                               
+# http://http://bower.io/ for details                                                                                                                                                                                                        
+npm install -g bower
+
+# Install Yeoman for project scaffolding
+npm install -g yo
+
+# # Install Heroku toolbelt                                                                                                                                                                                                                               
+# # https://toolbelt.heroku.com/                                                                                                                                                                                                                    
+wget -qO- https://toolbelt.heroku.com/install.sh | sh
